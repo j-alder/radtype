abc940081a399a5589708a701834fda1170c909a
Jonathon Alder <jonathon.alder@missionlane.com>
2023-03-13 15:04:25 -0400

    display letter on screen; remove String cast

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
 use piston::{WindowSettings, Position, Events, EventSettings, EventLoop, RenderEvent};
 use piston_window::{*, types::Color};
 
+fn key_to_string(k: Key, fallback: &str) -> &str {
+    match k.code(){
+        0x61=>"A",
+        0x62=>"B",
+        0x63=>"C",
+        0x64=>"D",
+        0x65=>"E",
+        0x66=>"F",
+        0x67=>"G",
+        0x68=>"H",
+        0x69=>"I",
+        0x6A=>"J",
+        0x6B=>"K",
+        0x6C=>"L",
+        0x6D=>"M",
+        0x6E=>"N",
+        0x6F=>"O",
+        0x70=>"P",
+        0x71=>"Q",
+        0x72=>"R",
+        0x73=>"S",
+        0x74=>"T",
+        0x75=>"U",
+        0x76=>"V",
+        0x77=>"W",
+        0x78=>"X",
+        0x79=>"Y",
+        0x7A=>"Z",
+        _=>fallback
+    }
+}
+
 fn draw_text(
     ctx: &Context,
     graphics: &mut G2d,
@@ -42,18 +74,24 @@ fn main() {
 
     let mut events = Events::new(EventSettings::new()).lazy(true);
 
-    let mut display_text = String::from("Hello world!");
+    let mut display_text = "";
 
     while let Some(e) = events.next(&mut window) {
         if let Some(_args) = e.render_args() {
             window.draw_2d(&e, |ctx, g, d| {
                 clear([1.0; 4], g);
-                draw_text(&ctx, g, &mut glyphs, [0.0, 0.0, 0.0, 1.0], Position { x: 0, y: 500 }, &display_text.as_str());
+                draw_text(
+                    &ctx, 
+                    g, 
+                    &mut glyphs, 
+                    [0.0, 0.0, 0.0, 1.0], 
+                    Position { x: 500, y: 500 }, 
+                    display_text);
                 glyphs.factory.encoder.flush(d)
             });
         }
         if let Some(Button::Keyboard(key)) = e.press_args() {
-            display_text = key.code().to_string();
+            display_text = key_to_string(key, &display_text);
         }
     }
 }
