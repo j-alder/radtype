83e21b51b29c6771cf34bb55cbd322d13d04e0ff
j-alder <jonathon.alder@gmail.com>
2023-03-30 08:37:43 -0400

    move camera bundle init to main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 mod text;
+mod gabe;
 
 use bevy::{prelude::*, DefaultPlugins};
+use gabe::GabePlugin;
 use text::TextPlugin;
+
+/** create a 2d camera */
+fn startup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
 
 fn main() {
     App::new()
+        .add_startup_system(startup_camera)
         .add_plugins(DefaultPlugins)
+        .add_plugin(GabePlugin)
         .add_plugin(TextPlugin)
         .run();
 }
